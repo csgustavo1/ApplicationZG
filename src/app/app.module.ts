f830e9f7c2f9ce69dbec66daf35ab465cf932671
csgustavo1 <gustavohj039@gmail.com>
2020-12-07 01:55:39 -0300

    tela de login criada, configuraçao de routas realizada, mudanças no InitialPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { CadastroUsuarioComponent } from './components/usuarios/cadastro-usuario/cadastro-usuario.component';
+import { routing } from './initial-page/app-routes';
+
+declare module "@angular/core" {
+  interface ModuleWithProviders<T = any> {
+    ngModule: Type<T>;
+    providers?: Provider[];
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    InitialPageComponent
+    InitialPageComponent,
+    CadastroUsuarioComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, InitialPageComponent, CadastroUsuarioComponent]
 })
 export class AppModule { }
